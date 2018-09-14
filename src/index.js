d1df263d7d9079c10a18d3d7fed363b61cd14c49
Rick42Morty <evgenevgen00@mail.ru>
2018-09-14 21:45:23 +0300

    Added my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var zeros = 0;
+  //Заметим, что 5^12 уже больше нашего максимума из условия
+  for (var n = 11; n > 0; n--){
+    zeros += Math.floor(number/Math.pow(5,n));
+  }
+
+  return zeros;
+
 }
